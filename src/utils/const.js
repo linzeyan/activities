e8e1c9c759c1d22be0d36abf5de4159229080f8c
Ricky <zeyanlin@outlook.com>
2024-06-21 20:43:33 +0800

    modify

--- src/utils/const.js
+++ src/utils/const.js
@@ -27,15 +27,15 @@ const IS_CHINESE = false;
 const USE_ANIMATION_FOR_GRID = true;
 const CHINESE_INFO_MESSAGE = (yearLength, year) => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据`;
+  return `我用 App 記錄自己跑步 ${yearLength} 年了，下麵列表展示的是${yearStr}的數據`;
 }
 const ENGLISH_INFO_MESSAGE = (yearLength, year) =>
   `${yearLength}-year-activity, the table shows year ${year} data`;
 
 // not support English for now
 const CHINESE_LOCATION_INFO_MESSAGE_FIRST =
-  '我跑过了一些地方，希望随着时间推移，地图点亮的地方越来越多';
-const CHINESE_LOCATION_INFO_MESSAGE_SECOND = '不要停下来，不要停下奔跑的脚步';
+  '我跑過了一些地方，希望隨著時間推移，地圖點亮的地方越來越多';
+const CHINESE_LOCATION_INFO_MESSAGE_SECOND = '不要停下來，不要停下奔跑的腳步';
 
 const INFO_MESSAGE = IS_CHINESE ? CHINESE_INFO_MESSAGE : ENGLISH_INFO_MESSAGE;
 
